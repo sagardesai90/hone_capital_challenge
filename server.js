4ec56966e72e7c7dd133ee890681d40076361111
sagardesai90 <saagardesai@gmail.com>
2020-01-13 16:39:30 -0800

    set up server.js

---
+++ server.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const path = require("path");
+const PORT = process.env.port || 3000;
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static(__dirname + "/public"));
+
+app.use(function(req, res) {
+  res.sendFile(path.join(__dirname + "/public/index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on localhost:${PORT}`);
+});
