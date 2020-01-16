0c75c8021cd1e6b3bc7510f589122e0a0eedbc5f
sagardesai90 <saagardesai@gmail.com>
2020-01-15 22:15:14 -0800

    got graph and two data tables to render, still need style

--- public/app.js
+++ public/app.js
@@ -1,9 +1,5 @@
-const https = require("https");
-const fetch = require("node-fetch");
-require("dotenv").config();
-
 //returns a list of top 10 coins
-async function getTopCoins() {
+async function getTopMarkets() {
   try {
     let response = await fetch("https://api.coinranking.com/v1/public/markets");
     let items = await response.json();
@@ -14,6 +10,24 @@ async function getTopCoins() {
     // console.log(filteredRes, "filteredRes");
   } catch (e) {
     console.log(e, "e");
+    return e;
+  }
+}
+
+async function getTopCoins() {
+  try {
+    let response = await fetch("https://api.coinranking.com/v1/public/coins");
+    let items = await response.json();
+    // console.log(items, "items");
+    const {
+      data: { coins }
+    } = items;
+    let filterRes = coins.filter(res => res.rank <= 10);
+    // console.log(filterRes, "top coins");
+    createCoinRow(filterRes);
+    chartRender(filterRes);
+  } catch (e) {
+    console.log(e);
     return e;
   }
 }
@@ -29,7 +43,7 @@ async function getExchanges() {
     } = topExchanges;
     let filteredRes = exchanges.filter(res => res.rank <= 10);
     // console.log(filteredRes, "filteredRes");
-    createRow(filteredRes);
+    createExchangeRow(filteredRes);
   } catch (e) {
     console.log(e, "e");
     return e;
@@ -38,15 +52,92 @@ async function getExchanges() {
 
 getExchanges();
 
-const createRow = res => {
+const createExchangeRow = res => {
   let rows = [];
-  //   console.log(res, "in createRow");
   for (let i = 0; i < res.length; i++) {
-    // let newRow = $("<tr>").append($("<td>").text(res[i].name));
-    let newRow = `<td class="data-row">` + res[i].name + `</td>`;
-    rows.push(newRow);
+    let row = [];
+    let rankRow = `<tr id="table-row"> <td>` + res[i].rank + `</td>`;
+    let nameRow = `<td>` + res[i].name + `</td>`;
+    let marketShareRow = `<td>` + res[i].marketShare + `</td></tr>`;
+    row.push(rankRow);
+    row.push(nameRow);
+    row.push(marketShareRow);
+    rows.push(row);
   }
-  console.log(rows, "rows");
-  document.getElementById("table-data").innerHTML = newRow[0];
+  for (let i = 0; i < rows.length; i++) {
+    document.getElementById("exchange-data").innerHTML += rows[i];
+  }
 };
+
+const createCoinRow = res => {
+  let rows = [];
+  for (let i = 0; i < res.length; i++) {
+    let row = [];
+    let rankRow = `<tr id="table-row"> <td>` + res[i].rank + `</td>`;
+    let nameRow = `<td>` + res[i].name + `</td>`;
+    let marketShareRow = `<td>` + res[i].price + `</td></tr>`;
+    let symbolRow = `<td>` + res[i].symbol + `</td>`;
+    row.push(rankRow);
+    row.push(symbolRow);
+    row.push(nameRow);
+    row.push(marketShareRow);
+    rows.push(row);
+  }
+  for (let i = 0; i < rows.length; i++) {
+    document.getElementById("coin-data").innerHTML += rows[i];
+  }
+};
+
+function chartRender(res) {
+  var ctx = document.getElementById("graph1");
+  console.log(res, "res in chart");
+  let labels = [];
+  let prices = [];
+  for (let i = 0; i < res.length; i++) {
+    labels.push(res[i].name);
+    prices.push(res[i].price);
+  }
+  console.log(prices, "prices");
+  var myChart = new Chart(ctx, {
+    type: "bar",
+    data: {
+      labels: labels,
+      datasets: [
+        {
+          label: "Price",
+          data: prices,
+          backgroundColor: [
+            "rgba(255, 99, 132, 0.2)",
+            "rgba(54, 162, 235, 0.2)",
+            "rgba(255, 206, 86, 0.2)",
+            "rgba(75, 192, 192, 0.2)",
+            "rgba(153, 102, 255, 0.2)",
+            "rgba(255, 159, 64, 0.2)"
+          ],
+          borderColor: [
+            "rgba(255, 99, 132, 1)",
+            "rgba(54, 162, 235, 1)",
+            "rgba(255, 206, 86, 1)",
+            "rgba(75, 192, 192, 1)",
+            "rgba(153, 102, 255, 1)",
+            "rgba(255, 159, 64, 1)"
+          ],
+          borderWidth: 1
+        }
+      ]
+    },
+    options: {
+      scales: {
+        yAxes: [
+          {
+            ticks: {
+              beginAtZero: true
+            }
+          }
+        ]
+      }
+    }
+  });
+}
+getTopMarkets();
 getTopCoins();
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const path = require("path");
-const PORT = process.env.port || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
